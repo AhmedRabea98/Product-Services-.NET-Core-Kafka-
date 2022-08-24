90df735e0f9f8292a41b52106ef26791b7fd8b3f
AhmedRabea98 <42179535+AhmedRabea98@users.noreply.github.com>
2022-08-24 14:41:09 +0200

    Add A new Producer Service

---
+++ Project/ProductService/ProductService2/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductService2.Models;
+using System.Net;
+using System.Text.Json;
+
+namespace ProductService2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly string bootstrapServers = "localhost:9092";
+        private readonly string topic = "simpletalk_topic";
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Product product)
+        {
+            string messsage = JsonSerializer.Serialize(product);
+            return Ok(await SendOrderRequest(topic, messsage));
+        }
+
+        public async Task<bool> SendOrderRequest(string topic, string message)
+        {
+            ProducerConfig config = new ProducerConfig
+            {
+
+                BootstrapServers = bootstrapServers,
+                ClientId = Dns.GetHostName(),
+
+            };
+            try
+            {
+                using (var producer = new ProducerBuilder<Null, string>(config).Build())
+                {
+
+                    var result = await producer.ProduceAsync(topic, new Message<Null, string> { Value = message });
+                    Console.WriteLine($"Delivery time {result.Timestamp.UtcDateTime}");
+                    return await Task.FromResult(true);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {ex.Message}");
+            }
+            return await Task.FromResult(false);
+        }
+
+    }
+}
---
+++ Project/ProductService/ProductService2/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductService2.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
